5b38337c1955a695b57496a4b4a9045cd66389b3
ademola olukotun <icuteade@gmail.com>
2024-10-09 01:36:04 -0700

    nav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const navlinks = [
 const Navbar = () => {
   return (
     <nav className=" fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto py-4">
         <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
           Ademola-Cyber
         </Link>
