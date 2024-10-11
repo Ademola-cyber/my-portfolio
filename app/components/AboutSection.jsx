0143cb15d85652365c5c6662686343e7bc0b7601
ademola olukotun <icuteade@gmail.com>
2024-10-11 09:37:54 -0700

    tabbutton

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,109 @@
+"use client";
+import Image from "next/image";
+import React, { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "Skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.Js</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>Sequelize</li>
+        <li>Javascript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+
+  {
+    title: "Eduucation",
+    id: "Education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>University of California, Santa Cruz</li>
+      </ul>
+    ),
+  },
+
+  {
+    title: "Certifications",
+    id: "Certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("Skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 items-center py-8 px-4 xl:gap-16 sm:py-16 gap-7">
+        <Image
+          src="/MOSHH.png"
+          width={500}
+          height={500}
+          className="rounded-md"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+            laboriosam incidunt quae hic nam aperiam magnam optio, fugit, nulla
+            omnis est facere voluptates soluta ex, aspernatur nostrum. Nisi
+            saepe rem consequuntur, tempora quo voluptatem porro minima corrupti
+            culpa et dolore maiores unde, exercitationem numquam esse sapiente
+            quibusdam. Non, quia tenetur.
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("Skills")}
+              active={tab === "Skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+
+            <TabButton
+              selectTab={() => handleTabChange("Education")}
+              active={tab === "Education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+
+            <TabButton
+              selectTab={() => handleTabChange("Certifications")}
+              active={tab === "Certifications"}
+            >
+              {" "}
+              Certification{" "}
+            </TabButton>
+          </div>
+
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Herosection from "./components/Herosection";
 import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar/>
       <div className="container mx-auto py-4 px-12 mt-24">
         <Herosection />
+        <AboutSection/>
       </div>
     </main>
   );
